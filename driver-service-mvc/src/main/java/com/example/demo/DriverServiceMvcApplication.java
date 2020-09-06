a682e92bcd92a1cae33eca075dfdd2a2bc571c8c
Dattaprasad <Dattaprasad@DESKTOP-4P3O7G6>
2020-09-06 11:35:01 +0530

    @ModelAttribute @PostMapping @GetMapping

--- driver-service-mvc/src/main/java/com/example/demo/DriverServiceMvcApplication.java
+++ driver-service-mvc/src/main/java/com/example/demo/DriverServiceMvcApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.model.Driver;
 
 @SpringBootApplication
 public class DriverServiceMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DriverServiceMvcApplication.class, args);
+	}
+	
+	@Bean
+	public Driver driver() {
+		return new Driver();
 	}
 
 }
